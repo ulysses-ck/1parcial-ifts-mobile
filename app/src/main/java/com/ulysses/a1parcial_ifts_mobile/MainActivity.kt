eac7f997d7a3c05bcdc10685a878e18afd4b0fe1
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-04 18:01:57 -0300

    fix: buttons id

--- app/src/main/java/com/ulysses/a1parcial_ifts_mobile/MainActivity.kt
+++ app/src/main/java/com/ulysses/a1parcial_ifts_mobile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ulysses.a1parcial_ifts_mobile
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit private var rgbColor: EditText
-    lateinit private var result: EditText
+    lateinit private var result: TextView
     lateinit private var btnConvert: Button
 
 
@@ -23,6 +24,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnConvert = findViewById(R.id.btnConvert)
+        rgbColor = findViewById(R.id.etRgbColor)
+        result = findViewById(R.id.tvResult)
+
 
         btnConvert.setOnClickListener {
             val color = rgbColor.text.toString()
